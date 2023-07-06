d35ebd22ec44527a2c6ee7dc7764224cc2d78316
= <alpianfebriyansah@gmail.com>
2023-07-06 12:08:52 +0700

    Repair some

--- resources/views/pengajuan_penggajian/index.blade.php
+++ resources/views/pengajuan_penggajian/index.blade.php
@@ -18,11 +18,11 @@
             @csrf
             <div class="mb-3">
                 <label for="periode_start" class="form-label">Periode Absen Mulai</label>
-                <input type="date" name="periode_start" class="form-control" value="{{ date('Y-m-d') }}" min="{{ date('Y-m-d', strtotime('+1 days', strtotime($pengajuan_penggajian->periode_end))); }}">
+                <input type="date" name="periode_start" class="form-control" value="{{ date('Y-m-d') }}" min="{{ isset($pengajuan_penggajian) ? date('Y-m-d', strtotime('+1 days', strtotime($pengajuan_penggajian->periode_end))) : ''; }}">
             </div>
             <div class="mb-3">
                 <label for="periode_end" class="form-label">Periode Absen Akhir</label>
-                <input type="date" name="periode_end" class="form-control" value="{{ date('Y-m-d') }}" min="{{ date('Y-m-d', strtotime('+1 days', strtotime($pengajuan_penggajian->periode_end))); }}">
+                <input type="date" name="periode_end" class="form-control" value="{{ date('Y-m-d') }}" min="{{ isset($pengajuan_penggajian) ? date('Y-m-d', strtotime('+1 days', strtotime($pengajuan_penggajian->periode_end))) : ''; }}">
             </div>
             <div class="mb-3">
                 <label for="keterangan" class="form-label">Keterangan Pengajuan</label>
