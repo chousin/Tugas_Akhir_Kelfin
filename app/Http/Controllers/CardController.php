a5b210b852820acf25c07b939fc7f3727739a7de
= <alpianfebriyansah@gmail.com>
2023-07-07 16:24:28 +0700

    Add calender absen

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -4,14 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Presensi;
+
+use App\Models\Karyawan;
+
+use Auth;
+
 class CardController extends Controller
 {
     public function index()
     {
+        $karyawan = Karyawan::all()->where('id_user', Auth::user()->id)->first();
+        $presensi = Presensi::all()->where('id_karyawan', $karyawan->id_karyawan);
+
+        $result = [];
+        foreach($presensi as $get_presensi){
+            $result_array = [
+                'title' => 'ABSEN',
+                'start' => substr($get_presensi->tanggal_masuk, 0, 10),
+                'end' => substr($get_presensi->tanggal_pulang, 0 , 10)
+            ];
+
+            array_push($result, $result_array);
+        }
+
+        $data_absen = json_encode($result);
+
         return view('components.card', [
             "title" => "Dashboard",
             "halaman" => "Home",
-            "sub_hal" => "Dashboard"
+            "sub_hal" => "Dashboard",
+            "data_absen" => $data_absen
         ]);
     }
 }
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -288,11 +288,24 @@
     <div class="row">
         <div class="col-12">
             <div class="card">
+                <input type="hidden" id="data-absen" value="{{ $data_absen }}">
                 <div class="card-body">
-                    
+                    <div id='calendar'></div>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var data_absen = document.getElementById('data-absen').value;
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          events: JSON.parse(data_absen)
+        });
+        calendar.render();
+      });
+    </script>
     @endif
 @endsection
